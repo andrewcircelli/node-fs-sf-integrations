bb3cc2d5e629c8d3dd3da971f0acd693a66920ab
Andrew Circelli <andrewcircelli@gmail.com>
2022-05-27 18:28:32 -0400

    added simple route to get /account/:id and return json res with error handle

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ config();
 const { SF_LOGIN_URL, SF_USERNAME, SF_PASSWORD, SF_TOKEN } = process.env;
 
 // init sf connection and login
-const connection = new jsforce.Connection({
+const conn = new jsforce.Connection({
   loginURL: SF_LOGIN_URL,
 });
-connection.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, (err, userInfo) => {
+conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, (err, userInfo) => {
   if (err) {
-    console.error(err);
+    return console.error(err);
   } else {
     console.log(
       `login successful for user id: ${userInfo.id} on organization id: ${userInfo.organizationId}`
@@ -33,7 +33,18 @@ const app = express();
 // Routes
 // =============================================================
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
+  res.status(200).send("Home Page");
+});
+app.get("/account/:id", (req, res) => {
+  conn.sobject("Account").retrieve(req.params.id, (err, account) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(404)
+        .send(`<h1> Invalid Request Made to /account/:id endpoint </h1>`);
+    }
+    res.status(200).send(account);
+  });
 });
 
 app.listen(PORT, () => {
